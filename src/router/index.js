2badb22c9ada5a7a22966dbd6d60f94a2ef5bc5d
mimiri <wmilkwpink@gmail.com>
2026-05-18 20:20:20 -0600

    elimine el registro de la plantilla

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
 import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
@@ -10,12 +9,6 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
-      meta: { guest: true },
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView,
       meta: { guest: true },
     },
     {
